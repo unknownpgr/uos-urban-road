55473ab5f39bb60f950dd59c2f1d0a3281059862
unknownpgr <unknownpgr@gmail.com>
2020-09-01 07:58:56 +0900

    Implement clean login

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,17 +3,35 @@ import AppContext from "../Context/AppContext";
 import { Redirect } from "react-router-dom";
 import axios from "axios";
 
-function App() {
-  let context = useContext(AppContext);
-  if (context.token) {
-    return (
-      <div>
-        <h1>Token</h1>
-        <div>{context.token}</div>
-      </div>
-    );
+class App extends React.Component {
+  static contextType = AppContext;
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "",
+    };
   }
-  return <Redirect to="/login"></Redirect>;
+
+  componentDidMount() {
+    axios
+      .get(
+        "http://web-dev.iptime.org:3001/api/username?token=" +
+          this.context.token
+      )
+      .then((res) => {
+        if (res.data.status !== "ok") {
+          this.context.setToken(undefined);
+        } else this.setState({ username: res.data.data });
+      });
+  }
+
+  render() {
+    // Go to login page if user is not logged in.
+    if (!this.context.token) return <Redirect to="/login"></Redirect>;
+    return <div>Main page</div>;
+  }
 }
 
 export default App;
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -54,12 +54,14 @@ class Login extends React.Component {
     if (token) {
       // If login is successful, set a token and redirect to the main page.
       this.context.setToken(token);
+      // The redirection occurs immediately after this code is executed.
+      // Therefore, component state update after this code will make warning.
       this.setState({ redirect: <Redirect to="/"></Redirect> });
     } else {
       // If login fails, show error message.
       this.setState({ err: "계정이 존재하지 않거나 비밀번호가 틀렸습니다." });
+      this.setState({ isLoading: false });
     }
-    this.setState({ isLoading: false });
   }
 
   render() {
@@ -68,6 +70,8 @@ class Login extends React.Component {
         <div className="background"></div>
         <Container className="login">
           {this.state.redirect}
+
+          {/* Main card */}
           <Card className="loginCard">
             <Card.Body>
               <img src="./img/logo.png" className="w-75" alt="한국도로공사" />
@@ -75,9 +79,12 @@ class Login extends React.Component {
                 한국도로공사 토공 다짐도 자동화 시스템
               </Card.Title>
               <Form onSubmit={this.handleLogin}>
+                {/* Error alert message */}
                 {this.state.err ? (
                   <Alert variant={"danger"}>{this.state.err}</Alert>
                 ) : undefined}
+
+                {/* ID input field */}
                 <Form.Group>
                   <Form.Label>User ID</Form.Label>
                   <Form.Control
@@ -87,6 +94,8 @@ class Login extends React.Component {
                     disabled={this.state.isLoading}
                   ></Form.Control>
                 </Form.Group>
+
+                {/* Password input field */}
                 <Form.Group>
                   <Form.Label>Password</Form.Label>
                   <Form.Control
@@ -96,6 +105,8 @@ class Login extends React.Component {
                     disabled={this.state.isLoading}
                   ></Form.Control>
                 </Form.Group>
+
+                {/* Submit button */}
                 <Form.Control
                   type="submit"
                   className="btn btn-primary"
