6d6cb7061d130059e38a5ab755954269aeaa1e73
unknownpgr <unknownpgr@gmail.com>
2020-11-18 18:49:19 +0000

    Move VideoPlayer into VideoViewer directory

--- src/components/VideoViewer/VideoViewer.jsx
+++ src/components/VideoViewer/VideoViewer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import VideoPlayer from "../VideoPlayer/VideoPlayer";
+import VideoPlayer from "./VideoPlayer/VideoPlayer";
 import "./videoViewer.scss";
 
 function VideoViewer() {
