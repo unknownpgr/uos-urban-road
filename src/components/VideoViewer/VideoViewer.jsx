2b32d9abdbf7973e1da2515cfed9715b40fe7e96
unknownpgr <unknownpgr@gmail.com>
2020-11-22 01:31:09 +0000

    Add snapshot setting UI

--- src/components/VideoViewer/VideoViewer.jsx
+++ src/components/VideoViewer/VideoViewer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, Form } from "react-bootstrap";
 import VideoPlayer from "./VideoPlayer/VideoPlayer";
 import "./videoViewer.scss";
 
@@ -7,7 +8,46 @@ function VideoViewer() {
     <table className="videoViewer">
       <tbody>
         <tr>
-          <td></td>
+          <td>
+            <Form className="mb-1">
+              <h4>스냅샷</h4>
+              <Form.Group className="mr-3">
+                <Form.Label for="snapshotTerm">저장 주기</Form.Label>
+                <Form.Control
+                  type="number"
+                  id="snapshotTerm"
+                  placeholder="단위 : 초(second)"
+                ></Form.Control>
+              </Form.Group>
+              <Form.Group className="mx-1 row">
+                <Form.Check
+                  type="checkbox"
+                  className="mx-1"
+                  label="Top"
+                  checked="true"
+                ></Form.Check>
+                <Form.Check
+                  type="checkbox"
+                  className="mx-1"
+                  label="Left"
+                  checked="true"
+                ></Form.Check>
+                <Form.Check
+                  type="checkbox"
+                  className="mx-1"
+                  label="Right"
+                  checked="true"
+                ></Form.Check>
+                <Form.Check
+                  type="checkbox"
+                  className="mx-1"
+                  label="Bottom"
+                  checked="true"
+                ></Form.Check>
+              </Form.Group>
+              <Button className="btn-primary">설정 저장</Button>
+            </Form>
+          </td>
           <td>
             <VideoPlayer label="top" src="/api/stream"></VideoPlayer>
           </td>
