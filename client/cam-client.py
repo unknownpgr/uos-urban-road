866f84400f3d452cf7b8a6a1568dca7509040907
unknownpgr <unknownpgr@gmail.com>
2020-11-27 08:18:48 +0900

    Add multiple-camera streaming function

--- client/cam-client.py
+++ client/cam-client.py
@@ -1,26 +1,70 @@
 import cv2
 import requests
+import numpy as np
 
-SEND_INTERVAL = 100
-SEND_INTERVAL_FAIL = 2000
+SEND_INTERVAL = 100  # ms
+SEND_INTERVAL_FAIL = 2000  # ms
+IMG_WIDTH = 320
+IMG_HEIGHT = 240
+CAMERA_INDEXES = [1, 2, 3, 4]
+SERVER_URL = "http://road.urbanscience.uos.ac.kr/api/stream"
 
-cap = cv2.VideoCapture(0)
-while True:
+# Create available camera dictinary
+cameras = {}
+for i in range(len(CAMERA_INDEXES)):
+    index = CAMERA_INDEXES[i]
+    cap = cv2.VideoCapture(index, cv2.CAP_DSHOW)
     ret, img = cap.read()
-    img = cv2.resize(img, (320, 240))
-    is_success, im_buf_arr = cv2.imencode(".jpg", img)
-    byte_im = im_buf_arr.tobytes()
-    file_dict = {"stream": byte_im}
+    if ret:
+        cameras[i] = cap
+        height, width, _ = img.shape
 
+        # Compare ratio to prevent distortion
+        if width*IMG_HEIGHT != IMG_WIDTH*height:
+            print(
+                f"Camera ratio and image ratio are different for camera {index}. cam={width/height},img={IMG_WIDTH/IMG_HEIGHT}")
+            exit(1)
+    else:
+        cap.release()
+
+# Pre-allocate arrays
+img_concat = np.zeros((IMG_HEIGHT*len(CAMERA_INDEXES), IMG_WIDTH, 3))
+img_empty = np.zeros((IMG_HEIGHT, IMG_WIDTH, 3))
+
+while True:
     try:
-        data = requests.post(
-            "http://road.urbanscience.uos.ac.kr/api/stream", files=file_dict)
+
+        # Read camera and make concatenated image
+        for i in cameras:
+            cap = cameras[i]
+            ret, img = cap.read()
+            if ret:
+                img = cv2.resize(img, (IMG_WIDTH, IMG_HEIGHT))
+                img_concat[IMG_HEIGHT*i: IMG_HEIGHT*(i+1), :] = img
+            else:
+                img_concat[IMG_HEIGHT*i: IMG_HEIGHT*(i+1), :] = img_empty
+
+        # Encode image into byte array
+        is_success, im_buf_arr = cv2.imencode(".jpg", img_concat)
+        byte_im = im_buf_arr.tobytes()
+        file_dict = {"stream": byte_im}
+
+        # Send image to server
+        data = requests.post(SERVER_URL, files=file_dict)
         cv2.waitKey(SEND_INTERVAL)
         print(data.json())
+
     except Exception:
+        # If image transmission failed, try after SEND_INTERVAL_FAIL ms.
         print(
             f"Stream transmission failed. Retry after {SEND_INTERVAL_FAIL}ms...")
         cv2.waitKey(SEND_INTERVAL_FAIL)
+
     except KeyboardInterrupt:
-        print('KILL signal detected. Shutdow client.')
+        # If CTRL+C pressed, break the loop
+        print('Shutdow client')
         break
+
+# Release all resources
+for i in cameras:
+    cameras[i].release()
