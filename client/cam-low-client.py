67372721b8086b40c14d8b7d1dc2481608eaea4d
unknownpgr <unknownpgr@gmail.com>
2021-05-11 16:08:00 +0900

    Add settings

--- client/cam-low-client.py
+++ client/cam-low-client.py
@@ -4,24 +4,32 @@ import numpy as np
 import time
 import sys
 
+print("Start client")
+
 SEND_INTERVAL_FAIL = 2  # unit : second
 IMG_WIDTH = 320
 IMG_HEIGHT = 240
-CAMERA_INDEXES = [1, 2, 3, 4]
+
+CAMERA_INDEXES = [0, 1, 2, 3]
 SERVER_URL = "https://api.road.urbanscience.uos.ac.kr/stream"
 
-# Redirect stderr to log file to remove warning message
-sys.stderr = open('.\err.log', 'w')
+if(len(sys.argv) > 1):
+    CAMERA_INDEXES = list(map(lambda x: int(x), sys.argv[1:]))
+    if len(CAMERA_INDEXES) > 4:
+        CAMERA_INDEXES = CAMERA_INDEXES[:4]
+
+print('Used camera :', CAMERA_INDEXES)
 
 # Pre-allocate arrays
 img_concat = np.zeros((IMG_HEIGHT*len(CAMERA_INDEXES), IMG_WIDTH, 3))
 img_empty = np.zeros((IMG_HEIGHT, IMG_WIDTH, 3))
 
 while True:
+    success = False
     try:
         # Read camera and make concatenated image
         for i in range(len(CAMERA_INDEXES)):
-            cap = cv2.VideoCapture(CAMERA_INDEXES[i])
+            cap = cv2.VideoCapture(CAMERA_INDEXES[i], cv2.CAP_DSHOW)
             ret, img = cap.read()
             if ret:
                 # Get image and update concatentated image
@@ -37,9 +45,13 @@ while True:
                 data = requests.post(SERVER_URL, files=file_dict)
                 if data.status_code == 200:
                     print(data.text, flush=True)
+                    success = True
             else:
                 img_concat[IMG_HEIGHT*i: IMG_HEIGHT*(i+1), :] = img_empty
             cap.release()
+
+        if not success:
+            time.sleep(3)
 
     except Exception as e:
         # If image transmission failed, try after SEND_INTERVAL_FAIL ms.
